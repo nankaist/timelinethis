dea7f3d82795b675f2275bb96076513b5dc78b9d
Will Chou <chou3213@gmail.com>
2011-12-18 12:07:17 -0500

    timelinethis first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Timelinethis::Application.config.secret_token = '600bf99412e473efc4ee8a4af969ec99e9d426dfc29dbd07028822c7f8ec1092db7df147ee9da493b484c37cce88960cb9556a5f5e22666cee8a4f48406adc97'
